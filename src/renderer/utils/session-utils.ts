ea630b2fc0e4467d7c28b010f5284dcca2e45571
themez <themezeng@gmail.com>
2025-06-03 21:24:58 +0800

    fix: session message list

--- src/renderer/utils/session-utils.ts
+++ src/renderer/utils/session-utils.ts
@@ -5,17 +5,17 @@ import { migrateMessage } from './message'
 export function migrateSession(session: Session): Session {
   return {
     ...session,
-    messages: session.messages?.map((m) => migrateMessage(m)),
+    messages: session.messages?.map((m) => migrateMessage(m)) || [],
     threads: session.threads?.map((t) => ({
       ...t,
-      messages: t.messages.map((m) => migrateMessage(m)),
+      messages: t.messages.map((m) => migrateMessage(m)) || [],
     })),
     messageForksHash: mapValues(session.messageForksHash || {}, (forks) => ({
       ...forks,
       lists: forks.lists?.map((list) => ({
         ...list,
-        messages: list.messages?.map((m) => migrateMessage(m)),
-      })),
+        messages: list.messages?.map((m) => migrateMessage(m)) || [],
+      })) || [],
     })),
   }
 }
