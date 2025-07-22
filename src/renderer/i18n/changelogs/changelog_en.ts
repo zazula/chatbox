e256b86463fd81e5aa70c44bd6d6d74ca6af8449
themez <themezeng@gmail.com>
2025-07-22 11:02:57 +0800

    release 1.15.1

--- src/renderer/i18n/changelogs/changelog_en.ts
+++ src/renderer/i18n/changelogs/changelog_en.ts
@@ -1,11 +1,12 @@
 const changelog = `
-## v1.15.1 - 2025.07.19
+## v1.15.1 - 2025.07.22
 
 1. Support disabling streaming output
 2. Support setting max token parameter
 3. Fix issue where mobile couldn't access ollama
 4. Adjust input box style
 5. Support importing MCP and provider configurations through deep link
+6. Fix issue where new conversation message was not sent in some conditions
 
 ### v1.15.0 - 2025.07.07
 
